8c4b434423ddf28cba0dba44ba80ee409ac7cb34
Nicke <nikolas.syverod@gmail.com>
2021-04-11 00:27:13 +0300

    Added a hook for user input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,10 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 export const Form = () => {
+  const [city, setCity] = useState("");
+
   return (
     <form className="flex justify-center">
       <div className="relative">
@@ -9,12 +12,13 @@ export const Form = () => {
           className="mt-64 h-11 w-104 rounded-xl pl-2 focus:shadow-lg outline-none"
           type="text"
           placeholder="Enter city"
+          onChange={(e) => setCity({ city: e.target.value })}
         />
         <FontAwesomeIcon
           icon={faSearch}
           className="bottom-0 absolute right-2 mb-3"
           size="lg"
-          onClick={() => console.log("click")}
+          onClick={() => console.log(city)}
         />
       </div>
     </form>
