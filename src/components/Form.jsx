9eb9359d24e4b5f4a3fc0e16bd132c33d3a14c1d
Nicke <nikolas.syverod@gmail.com>
2021-04-10 23:37:35 +0300

    Added a text input and a search icon inside it

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 export const Form = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <form className="flex justify-center">
+      <div className="relative">
+        <input
+          className="mt-64 h-11 w-104 rounded-xl pl-2 focus:shadow-lg outline-none"
+          type="text"
+          placeholder="Enter city"
+        />
+        <FontAwesomeIcon
+          icon={faSearch}
+          className="bottom-0 absolute right-2 mb-3"
+          size="lg"
+          onClick={() => console.log("click")}
+        />
+      </div>
+    </form>
+  );
+};
