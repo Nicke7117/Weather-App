2cb43b70e551b8118af531a698a03b168e5fae54
Nicke <nikolas.syverod@gmail.com>
2021-04-10 20:14:12 +0300

    Imported Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { Form } from "./components/Form"
 
-
-function App() {
+export const App = () => {
   return (
-    <div>
-
+    <div className="w-full h-full bg-gradient-to-t from-yellow-200 to-yellow-600 absolute">
+      <p className="ml-2">Weather app</p>
+      <Form />
+      
     </div>
   );
 }
 
-export default App;
+
