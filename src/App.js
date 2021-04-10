16e7f97d6eb037aed068b7a1586be3aefaf182a2
Nicke <nikolas.syverod@gmail.com>
2021-04-10 20:55:38 +0300

    Removed Form and added Main which will contain the main content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Form } from "./components/Form"
+import { Main } from "./components/Main"
 
 export const App = () => {
   return (
     <div className="w-full h-full bg-gradient-to-t from-yellow-200 to-yellow-600 absolute">
       <p className="ml-2">Weather app</p>
-      <Form />
+      <Main />
       
     </div>
   );
