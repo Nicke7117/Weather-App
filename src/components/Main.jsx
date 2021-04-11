c32f9666a7ff12379250014b1df61f9b1b6c7a78
Nicke <nikolas.syverod@gmail.com>
2021-04-12 02:00:11 +0300

    Removed the component that contained two components and added the components directly

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,18 @@
 import { Form } from "./Form";
-import { WeatherData } from "./WeatherData";
+import { MainWeather } from "./MainWeather";
+import { WeatherForecast } from "./WeatherForecast";
 
 export const Main = () => {
   return (
     <main className="justify-center flex">
       <div className="w-full">
         <Form />
-        <WeatherData />
+        <div className="flex justify-center">
+          <div className="lg:max-w-screen-lg w-full">
+            <MainWeather />
+            <WeatherForecast />
+          </div>
+        </div>
       </div>
     </main>
   );
