422125de1e8e84c31ce868f3456a2a4920329d4f
Nicke <nikolas.syverod@gmail.com>
2021-04-11 22:59:10 +0300

    Added padding to x-axis

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Main } from "./components/Main"
 
 export const App = () => {
   return (
-    <div className="w-full h-full bg-gradient-to-t from-yellow-200 to-yellow-600 absolute">
+    <div className="w-full h-full bg-gradient-to-t from-yellow-200 to-yellow-600 absolute px-2">
       <p className="ml-2">Weather app</p>
       <Main />
       
