4be727abcda6b498d9f58cdfa529e23723a64cd2
Nicke <nikolas.syverod@gmail.com>
2021-04-12 02:41:42 +0300

    imported apiKey.js

--- src/components/MainWeather.jsx
+++ src/components/MainWeather.jsx
@@ -1,14 +1,16 @@
 import axios from "axios";
+import { apiKey } from "../apiKey"
 
 export const MainWeather = () => {
   function getData() {
     axios
       .get(
-        "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=f6f246dbb9a0aa8b67c9b76d968808e6",
+        "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=" + apiKey,
         { params: { units: "metric" } }
       )
       .then((data) => console.log(data.data))
       .catch((e) => console.log(e));
   }
+  getData()
   return <div className="mt-11 h-11 mx-auto flex"></div>;
 };
