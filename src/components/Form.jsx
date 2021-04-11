cd9f0c14d317d5548be8fda34db644106ca30b30
Nicke <nikolas.syverod@gmail.com>
2021-04-11 23:01:35 +0300

    Made the search bar responsive

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
-
+/*  */
 export const Form = () => {
   const [city, setCity] = useState("");
 
   return (
     <form className="flex justify-center">
-      <div className="relative">
+      <div className="relative flex justify-center w-full sm:max-w-screen-sm">
         <input
-          className="mt-64 h-11 w-104 rounded-xl pl-2 focus:shadow-lg outline-none"
+          className="mt-64 h-11 w-full rounded-xl pl-2 focus:shadow-lg outline-none "
           type="text"
           placeholder="Enter city"
           onChange={(e) => setCity({ city: e.target.value })}
