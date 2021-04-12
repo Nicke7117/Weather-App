71ea78d09b973bbe81c5d8e35c37abbfff16be4c
Nicke <nikolas.syverod@gmail.com>
2021-04-12 19:05:30 +0300

    Deleted the Main component and replaced it with the content inside it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import { Main } from "./components/Main"
+import { Form } from "./components/Form";
+import { MainWeather } from "./components/MainWeather";
+import { WeatherForecast } from "./components/WeatherForecast";
 
 export const App = () => {
   return (
     <div className="w-full h-full bg-gradient-to-t from-yellow-200 to-yellow-600 absolute px-2">
       <p className="ml-2">Weather app</p>
-      <Main />
-      
+      <main className="justify-center flex">
+        <div className="w-full">
+          <Form />
+          <div className="flex justify-center">
+            <div className="lg:max-w-screen-lg w-full">
+              <MainWeather />
+              <WeatherForecast />
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
-}
-
-
+};
