5ab9b36870ebcbdc7e2e65f8358ef462d1ea9997
Nicke <nikolas.syverod@gmail.com>
2021-04-13 22:38:23 +0300

    Created custom grid rows

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {width: {"104": "26rem"}},
+    extend: { gridTemplateRows: { 2: "repeat(2, minmax(0, 120px))" } },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
