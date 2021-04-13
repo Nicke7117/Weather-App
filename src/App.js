2c8acd961aefaf336256290a9c8b5489a0dbcb2b
Nicke <nikolas.syverod@gmail.com>
2021-04-13 16:34:11 +0300

    Removed unnecessary divs and restructured html

--- src/App.js
+++ src/App.js
@@ -4,17 +4,13 @@ import { WeatherForecast } from "./components/WeatherForecast";
 
 export const App = () => {
   return (
-    <div className="w-full h-full bg-gradient-to-t from-yellow-200 to-yellow-600 absolute px-2">
+    <div className="w-full h-full bg-gradient-to-t from-yellow-200 to-yellow-500 absolute px-2 overflow-auto">
       <p className="ml-2">Weather app</p>
-      <main className="justify-center flex">
-        <div className="w-full">
+      <main className="w-full flex justify-center">
+        <div className="lg:max-w-screen-lg w-full">
           <Form />
-          <div className="flex justify-center">
-            <div className="lg:max-w-screen-lg w-full">
-              <MainWeather />
-              <WeatherForecast />
-            </div>
-          </div>
+          <MainWeather />
+          <WeatherForecast />
         </div>
       </main>
     </div>
