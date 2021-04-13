527b748e4601709cb0da611bb9c8f165ca9b9a1f
Nicke <nikolas.syverod@gmail.com>
2021-04-13 16:37:30 +0300

    Removed unused classes and made some fine-tuning

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,16 +7,16 @@ export const Form = () => {
 
   return (
     <form className="flex justify-center">
-      <div className="relative flex justify-center w-full sm:max-w-screen-sm">
+      <div className="relative w-full sm:max-w-screen-sm">
         <input
-          className="mt-64 h-11 w-full rounded-xl pl-2 focus:shadow-lg outline-none "
+          className="mt-28 h-11 w-full rounded-full pl-2 focus:shadow-lg outline-none "
           type="text"
           placeholder="Enter city"
           onChange={(e) => setCity({ city: e.target.value })}
         />
         <FontAwesomeIcon
           icon={faSearch}
-          className="bottom-0 absolute right-2 mb-3"
+          className="bottom-0 absolute right-3 mb-3"
           size="lg"
           onClick={() => console.log(city)}
         />
