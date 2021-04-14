4c9fcf6efc1e31408c51ddfe0bffb61aa0ce6e57
Nicke <nikolas.syverod@gmail.com>
2021-04-14 19:38:18 +0300

    Added a section tag

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { WeatherForecast } from "./components/WeatherForecast";
 
 export const App = () => {
   return (
-    <div className="w-full h-full bg-gradient-to-t from-yellow-200 to-yellow-500 absolute px-2 overflow-auto">
+    <div className="w-full h-full bg-gradient-to-t from-yellow-300 to-yellow-600 absolute px-2 overflow-auto">
       <p className="ml-2">Weather app</p>
       <main className="w-full flex justify-center">
         <div className="lg:max-w-screen-lg w-full">
           <Form />
-          <MainWeather />
-          <WeatherForecast />
+          <section className="max-w-xl text-center mx-auto p-5 relative mt-10">
+            <MainWeather />
+            <WeatherForecast />
+          </section>
         </div>
       </main>
     </div>
