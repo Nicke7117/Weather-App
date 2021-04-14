6355e0172e824474a5863fc2d2f41a7cde864d68
Nicke <nikolas.syverod@gmail.com>
2021-04-15 02:03:49 +0300

    Added two hooks and a useEffect when the search is different

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
-/*  */
-export const Form = () => {
+import { useState, useEffect } from "react";
+import { apiKey } from "../apiKey";
+import axios from "axios";
+
+export const Form = ({ setWeather }) => {
+  const [input, setInput] = useState("");
+
   const [city, setCity] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&appid=" +
+          apiKey,
+        { params: { units: "metric" } }
+      )
+      .then((data) =>
+        setWeather(data.data)
+      )
+      .catch((e) => console.log("Error! " + e));
+  }, [city]);
 
   return (
     <form className="flex justify-center">
@@ -12,13 +31,13 @@ export const Form = () => {
           className="mt-28 h-11 w-full rounded-full pl-2 focus:shadow-lg outline-none "
           type="text"
           placeholder="Enter city"
-          onChange={(e) => setCity({ city: e.target.value })}
+          onChange={(e) => setInput(e.target.value)}
         />
         <FontAwesomeIcon
           icon={faSearch}
           className="bottom-0 absolute right-3 mb-3"
           size="lg"
-          onClick={() => console.log(city)}
+          onClick={() => setCity(input)}
         />
       </div>
     </form>
