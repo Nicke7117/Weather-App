1ab7295e1ab1e0a8777d78b1037ce13ebbd2bd1c
Nicke <nikolas.syverod@gmail.com>
2021-04-16 01:45:13 +0300

    Added conditional if the weather components should be displayed, and added forecast state

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import { useState } from "react";
 
 export const App = () => {
   const [weather, setWeather] = useState("");
+  const [forecast, setForecast] = useState("");
 
   return (
     <div className="w-full h-full bg-gradient-to-t from-yellow-300 to-yellow-600 absolute px-2 overflow-auto">
       <p className="ml-2">Weather app</p>
       <main className="w-full flex justify-center">
         <div className="lg:max-w-screen-lg w-full">
-          <Form setWeather={setWeather} />
-          {weather !== "" ? (
-            <section className="max-w-xl text-center mx-auto p-5 relative mt-10">
-              <MainWeather />
-              <WeatherForecast />
-            </section>
-          ) : (
-            <p className="text-center mt-10 text-2xl">Search for a city!</p>
-          )}
+          <section className="max-w-xl text-center mx-auto p-5 relative mt-4">
+            <Form setWeather={setWeather} setForecast={setForecast} />
+            {weather !== "" ? (
+              <>
+                <MainWeather weather={weather} />
+                <WeatherForecast weather={weather} />{" "}
+              </>
+            ) : (
+              <p className="text-center mt-10 text-2xl">Search for a city!</p>
+            )}
+          </section>
         </div>
       </main>
     </div>
