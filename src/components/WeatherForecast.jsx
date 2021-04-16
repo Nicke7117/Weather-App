d50d182c20a262523f00aec66c773e70a11538c3
Nicke <nikolas.syverod@gmail.com>
2021-04-16 18:30:29 +0300

    Added a map method which loops through the weather forecast

--- src/components/WeatherForecast.jsx
+++ src/components/WeatherForecast.jsx
@@ -1,9 +1,16 @@
 import { WeatherList } from "./WeatherList";
 
-export const WeatherForecast = () => {
+export const WeatherForecast = ({ forecast }) => {
   return (
     <div className="bg-yellow-100 rounded-xl mt-2 overflow-x-auto p-2 flex flex-nowrap">
-      <WeatherList />
+      
+      
+      {forecast.map((forecast, index) => { 
+        return(
+      <WeatherList forecast={forecast} key={index}/>
+      )
+}
+      )}
     </div>
   );
 };
