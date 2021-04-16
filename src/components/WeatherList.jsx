0558aea8934ddffc0e6cb23fcbcc740c665efa70
Nicke <nikolas.syverod@gmail.com>
2021-04-16 18:28:00 +0300

    Added forecast data to html tags

--- src/components/WeatherList.jsx
+++ src/components/WeatherList.jsx
@@ -1,10 +1,12 @@
-export const WeatherList = () => {
+import { unixToTime } from "../unixToTime"
+
+export const WeatherList = ({ forecast }) => {
   return (
     <div className="rounded-xl py-2 px-5 border-2 border-black inline-block float-left mr-2 sm:px-6">
-      <p className="font-semibold text-lg sm:text-xl">{/* Date */}</p>
-      <p className="font-semibold text-lg sm:text-xl">{/* Time */}</p>
-      <img src="https://openweathermap.org/img/w/04n.png" alt=""/>
-      <p className="font-semibold text-xl sm:text-2xl">{/* Temperature */}</p>
+      <p className="font-semibold text-lg sm:text-xl">{forecast["dt_txt"].split(" ", 1)}</p>
+      <p className="font-semibold text-lg sm:text-xl">{unixToTime(forecast.dt)}</p>
+      <img src={"https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png"} alt="" className="mx-auto"/>
+      <p className="font-semibold text-xl sm:text-2xl">{forecast.main.temp}°</p>
     </div>
   );
 };
