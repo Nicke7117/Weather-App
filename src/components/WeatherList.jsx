d504ea7891c4b536af76caac25577fd730a5801d
Nicke <nikolas.syverod@gmail.com>
2021-04-17 15:56:00 +0300

    Added shadow and a bigger shadow when hovered

--- src/components/WeatherList.jsx
+++ src/components/WeatherList.jsx
@@ -2,7 +2,7 @@ import { unixToTime } from "../unixToTime"
 
 export const WeatherList = ({ forecast }) => {
   return (
-    <div className="rounded-xl py-2 px-5 border-2 border-black inline-block float-left mr-2 sm:px-6">
+    <div className="rounded-xl py-2 px-5 border-2 border-black inline-block float-left mr-2 sm:px-6 shadow-md hover:shadow-xl">
       <p className="font-semibold text-lg sm:text-xl">{forecast["dt_txt"].split(" ", 1)}</p>
       <p className="font-semibold text-lg sm:text-xl">{unixToTime(forecast.dt)}</p>
       <img src={"https://openweathermap.org/img/w/" + forecast.weather[0].icon + ".png"} alt="" className="mx-auto"/>
